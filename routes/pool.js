75da568ac467b8eebb689dcae2fcd06dbfb48cfb
Deepak Singh Tomar <112108549+deepaktomar3352@users.noreply.github.com>
2024-03-28 12:46:18 +0530

    update db

--- routes/pool.js
+++ routes/pool.js
@@ -1,11 +1,11 @@
 var mysql= require("mysql")
 var pool=mysql.createPool(
     {
-        host:'localhost',
+        host:'sql5.freesqldatabase.com',
         port:3306,
-        user:'root',
-        password:'3352',
-        database:'event',
+        user:'sql5694840',
+        password:'rhZZiVGXzq',
+        database:'sql5694840',
         multipleStatements:true,
         connectionLimit:100
     })
